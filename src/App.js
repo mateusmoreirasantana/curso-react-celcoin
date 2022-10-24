c56badbb2eec389abe9883a4732a8354f41b64b5
mateusmoreirasantana <82914631+mateusmoreirasantana@users.noreply.github.com>
2022-10-24 13:30:09 -0300

    Exercicio 1 - Café da manha

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  
+    <html lang="pt-br">
+    
+    <head>
+   
+      <title>Exercício - Café da manhã</title>
+    </head>
+    
+    <body>
+      <div class="root">
+        <div>
+          <h1>Lista de Compras</h1>
+          <ul>
+            <li>Pão</li>
+            <li>Banana</li>
+            <li>Milk Shake de Doce de Leite</li>
+            <li>Nutella</li>
+            <li>Sorvete</li>
+          </ul>
+        </div>
+      </div>
+    </body>
+    
+    </html>
+  );
+}
+
+export default App;
